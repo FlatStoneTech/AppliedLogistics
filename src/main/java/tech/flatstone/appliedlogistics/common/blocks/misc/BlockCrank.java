b2246c546922ceef20060b994b87c73815000a3f
Phylogeny <molecularphylo@gmail.com>
2016-05-04 17:48:38 -0700

    Formatting.

--- src/main/java/tech/flatstone/appliedlogistics/common/blocks/misc/BlockCrank.java
+++ src/main/java/tech/flatstone/appliedlogistics/common/blocks/misc/BlockCrank.java
@@ -312,11 +312,11 @@ public class BlockCrank extends BlockTileBase implements IProvideRecipe, IBlockR
             {
                 for (int l = 0; l < i; ++l)
                 {
-                	double d0 = pos.getX() + (j + 0.5D) / i;
+                    double d0 = pos.getX() + (j + 0.5D) / i;
                     double d1 = pos.getY() + (k + 0.5D) / i;
                     double d2 = pos.getZ() + (l + 0.5D) / i;
                     Vec3d vec = new Vec3d(d0, d1, d2);
-                	addMaskedDestroyEffects(pos, effectRenderer, stateID, vec, crankTop, crankShaft);
+                    addMaskedDestroyEffects(pos, effectRenderer, stateID, vec, crankTop, crankShaft);
                 }
             }
         }
@@ -327,10 +327,10 @@ public class BlockCrank extends BlockTileBase implements IProvideRecipe, IBlockR
         
         for (AxisAlignedBB mask : masks)
         {
-        	if (mask.isVecInside(vec))
+            if (mask.isVecInside(vec))
             {
-            	effectRenderer.spawnEffectParticle(EnumParticleTypes.BLOCK_CRACK.getParticleID(), vec.xCoord, vec.yCoord, vec.zCoord, vec.xCoord - pos.getX() - 0.5D, vec.yCoord - pos.getY() - 0.5D, vec.zCoord - pos.getZ() - 0.5D, stateID);
-            	break;
+                effectRenderer.spawnEffectParticle(EnumParticleTypes.BLOCK_CRACK.getParticleID(), vec.xCoord, vec.yCoord, vec.zCoord, vec.xCoord - pos.getX() - 0.5D, vec.yCoord - pos.getY() - 0.5D, vec.zCoord - pos.getZ() - 0.5D, stateID);
+                break;
             }
         }
     }
