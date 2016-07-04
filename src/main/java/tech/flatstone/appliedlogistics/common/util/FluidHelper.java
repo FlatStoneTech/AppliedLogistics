388e029444b21903a3e75c57fc6a203002af5e42
FireBall1725 <fireball@fireball1725.com>
2016-07-04 12:13:14 -0700

    test

--- src/main/java/tech/flatstone/appliedlogistics/common/util/FluidHelper.java
+++ src/main/java/tech/flatstone/appliedlogistics/common/util/FluidHelper.java
@@ -2,12 +2,15 @@ package tech.flatstone.appliedlogistics.common.util;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelBakery;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fluids.BlockFluidClassic;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.IFluidBlock;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.*;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import tech.flatstone.appliedlogistics.AppliedLogisticsCreativeTabs;
 import tech.flatstone.appliedlogistics.ModInfo;
@@ -30,6 +33,22 @@ public class FluidHelper {
         GameRegistry.register(fluidBlock);
         GameRegistry.register((new ItemBlock(fluidBlock)).setRegistryName(fluidBlock.getRegistryName()));
 
+        Item fluidItem = Item.getItemFromBlock(fluidBlock);
+        ModelBakery.registerItemVariants(fluidItem);
+
+        final String resourcePath = String.format("%s:fluids", ModInfo.MOD_ID, fluidBlock.getFluid().getName());
+        ModelLoader.setCustomStateMapper(fluidBlock, new StateMapperBase() {
+            @Override
+            protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+                return new ModelResourceLocation(resourcePath, getPropertyString(state.getProperties()));
+            }
+        });
+
         return fluid;
     }
+
+
+
+
+
 }
