ffcb9c9de4eb3c4c5aeae9e3a19707ae3ee26fb7
sp-guip <pelegbr@gmail.com>
2016-07-25 22:12:55 +0300

    Added a more accurate Percent Randomizer.
    Added a way to Randomize percent more accurately + slight increase of performance

--- src/main/java/tech/flatstone/appliedlogistics/common/util/RandomHelper.java
+++ src/main/java/tech/flatstone/appliedlogistics/common/util/RandomHelper.java
@@ -5,13 +5,24 @@ import net.minecraft.util.math.MathHelper;
 import java.util.Random;
 
 public class RandomHelper {
+    private static Ramdom rng = new Random();
     public static int CalculatePulverizer(float itemValue, float fortuneValue) {
-        Random rng = new Random();
         if (fortuneValue == 0) {
             return (int) Math.ceil(itemValue);
         } else {
             float random = MathHelper.randomFloatClamp(rng, itemValue, fortuneValue + itemValue);
             return (int) Math.ceil(random);
         }
+    }
+    
+    public static int calcyield(double itemchance, double fortunemultiplier) {
+	if (itemchance % 1 > 0.0d) {
+		double Bonuschance = (itemchance % 1) * 100;
+		double result =  (rng.nextFloat() * fortunemultiplier * 1 / ((100.d - Bonuschance) / 100.0d))/(itemchance * fortunemultiplier);
+		return (int) (Math.floor(itemchance) + result);
+	}
+
+    return (int) Math.floor(itemchance);
+
     }
 }
