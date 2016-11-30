cf573c577d28b84856d28311b07c86f9a792839e
FireBall1725 <fireball@fireball1725.com>
2016-11-29 21:10:32 -0800

    Furnace part 2

--- src/main/java/com/fireball1725/corelib/network/messages/PacketGuiObjectClicked.java
+++ src/main/java/com/fireball1725/corelib/network/messages/PacketGuiObjectClicked.java
@@ -1,4 +1,53 @@
-package com.fireball1725.corelib.guimaker.network.messages;
+package com.fireball1725.corelib.network.messages;
 
-public class PacketGuiObjectClicked {
+import com.fireball1725.corelib.guimaker.GuiMaker;
+import com.fireball1725.corelib.guimaker.GuiMakerContainer;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.inventory.Container;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import tech.flatstone.appliedlogistics.common.util.LogHelper;
+
+public class PacketGuiObjectClicked implements IMessage, IMessageHandler<PacketGuiObjectClicked, IMessage> {
+    private int objectID;
+    private int guiID;
+
+    public PacketGuiObjectClicked() {
+
+    }
+
+    public PacketGuiObjectClicked(int guiID, int objectID) {
+        this.objectID = objectID;
+        this.guiID = guiID;
+    }
+
+    @Override
+    public void fromBytes(ByteBuf buf) {
+        this.guiID = buf.readInt();
+        this.objectID = buf.readInt();
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+        buf.writeInt(this.guiID);
+        buf.writeInt(this.objectID);
+    }
+
+    @Override
+    public IMessage onMessage(final PacketGuiObjectClicked message, final MessageContext ctx) {
+        IThreadListener mainThread = (WorldServer)ctx.getServerHandler().playerEntity.worldObj;
+        mainThread.addScheduledTask(new Runnable() {
+            @Override
+            public void run() {
+                GuiMaker guiMaker = GuiMaker.getGuiMaker(message.guiID);
+                guiMaker.guiObjectClicked(message.objectID);
+            }
+        });
+
+        return null;
+    }
 }
